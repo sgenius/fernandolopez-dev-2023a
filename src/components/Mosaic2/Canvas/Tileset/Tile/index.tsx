9b6ecb77cc1c27a584cc77c7de2a5af6cc2683f6
Fernando Augusto López Plascencia <fa_lopez1@yahoo.com.mx>
2023-02-25 23:06:29 -0800

    put tiles in order

--- src/components/Mosaic2/Canvas/Tileset/Tile/index.tsx
+++ src/components/Mosaic2/Canvas/Tileset/Tile/index.tsx
@@ -10,8 +10,26 @@ const Mosaic2Tile: React.FC<TileKey> = ({ xGrid, yGrid, imageSet }) => {
     const fullPath = `${path}${xGrid},${yGrid}.jpg`;
     const [image, status] = useImage(fullPath);
 
+    const { pieceDimensions, bounds: {xMin, yMin, yMax} } = MOSAIC_DATA;
+    const width = pieceDimensions.width * pieceDimensions.multiplier;
+    const height = pieceDimensions.height * pieceDimensions.multiplier;
+
+    const xRef = (xGrid - xMin);
+    const yRange = yMax - xMin;
+    const yRef = yRange - yGrid + yMin;
+
+    const x = width * xRef;
+    const y = height * yRef;
+
     return status === 'loaded' ? (
-        <Image image={image} alt={`tile at ${xGrid}, ${yGrid}`} />
+        <Image
+            image={image}
+            alt={`tile at ${xGrid}, ${yGrid}`}
+            width={width}
+            height={height}
+            x={x}
+            y={y}
+        />
     ) : null;
 }
 
